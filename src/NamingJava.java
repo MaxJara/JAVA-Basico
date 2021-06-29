c6e636671bab0c61e3b0c57bf72b8a2ad1762c68
MaxJara <maxmata77@gmail.com>
2021-06-29 16:53:28 -0500

    Se agrego convencion de nombres

---
+++ src/NamingJava.java
@@ -0,0 +1,19 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 33337777;
+        int celPhone = 55553333;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Mexico";
+        String _backgroundColor = "Blue";
+
+        String currency$ = "MXN";
+        String background_color = "Green";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+    }
+}
