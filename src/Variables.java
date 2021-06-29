0a0871701e5a0c95b2f5031b934526cb3bc6d049
MaxJara <maxmata77@gmail.com>
2021-06-29 16:21:24 -0500

    Se utilizaron variables

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declarando una variable
+        int speed;
+        //Se asgino el valor a la variable
+        speed=10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Max Jara";
+        System.out.println(employeeName);
+
+    }
+}
