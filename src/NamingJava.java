7f4a44831491fae7da99fe686506902dbc6819b1
MaxJara <maxmata77@gmail.com>
2021-06-29 16:59:34 -0500

    Explicacion de Upper y Lower Camel Case

--- src/NamingJava.java
+++ src/NamingJava.java
@@ -1,3 +1,4 @@
+            //Upper Camel Case
 public class NamingJava {
     public static void main(String[] args) {
         int celphone = 33337777;
@@ -14,6 +15,10 @@ public class NamingJava {
         int POSITION = -5;
         int MAX_WIDTH = 9999;
         int MIN_WIDTH = 1;
+
+        //Lower Camel Case
+        String fullName = "Maximiliano Isaac Mata Jara";
+        int sizeInCentimeters = 26;
 
     }
 }
