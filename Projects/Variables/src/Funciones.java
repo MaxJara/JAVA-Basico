9dd6a7698f8787aae4d3a919d1b25178adbdc7b2
MaxJara <maxmata77@gmail.com>
2021-06-30 18:05:09 -0500

    Se javadoc a Funciones

--- Projects/Variables/src/Funciones.java
+++ Projects/Variables/src/Funciones.java
@@ -14,20 +14,46 @@ public class Funciones {
         sphereVolumen(y);
 
         System.out.println("PESOS A DOLARES: "+ converToDolar(200, "MXN"));
-    }
 
+
+    }
+    /**
+     * Descripcion: Funcion que te da el area del circulo
+     *
+     * @param r Radio del circulo
+     * @return Devuelve el are del circulo
+     * */
     public static double circleArea(double r){
         return Math.PI * Math.pow(r,2);
     }
 
+    /**
+     * Descripcion: Funcion que te da el area de la esfera
+     *
+     * @param r Radio de la esfera
+     * @return Devuelve el area de la esfera
+     * */
     public static double sphereArea(double r){
         return 4 * Math.PI * Math.pow(r,2);
     }
 
+    /**
+     * Descripcion: Funcion que te da el volumen de la esfera
+     *
+     * @param r Radio de la esfera
+     * @return Devuelve el volumen de la esfera
+     * */
     public static double sphereVolumen(double r){
         return  (4/3) * Math.PI * Math.pow(r,3);
     }
 
+    /**
+     * Descripcion: Funcion que especificando su moneda convierte una cantidad de dinero a dolares
+     *
+     * @param quantity Cantidad de dinero
+     * @param currency Tipo de Moneda: Solo acepta MXN o COP
+     * @return quantity Devuelve la cantidad actualizada en dolares
+     * */
     public static double converToDolar (double quantity, String currency) {
         //MXN
         switch (currency) {
