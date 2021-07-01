7e03918c7384b8a47d0b6e117ea9a9f9fa29390c
MaxJara <maxmata77@gmail.com>
2021-06-30 20:40:41 -0500

    Se utilizo if anidados en el archivo Arrays

--- Projects/Variables/src/Arrays.java
+++ Projects/Variables/src/Arrays.java
@@ -1,7 +1,7 @@
 public class Arrays {
     public static void main(String[] args) {
         String [] androidVersions = new String[17];
-        String days[] = new String[7];
+        int days[] = new int[7];
 
         String[][] cities =  new String[4][2];
 
@@ -13,10 +13,18 @@ public class Arrays {
         androidVersions[2] = "Cupcake";
         androidVersions[3] = "Donut";
 
+        for (int i = 0; i < androidVersions.length; i++) {
+            System.out.println(androidVersions[i]);
+        }
+        for (String androidVersion: androidVersions) {
+            System.out.println(androidVersion);
+        }
+
+        /*
         System.out.println(androidVersions[0]);
         System.out.println(androidVersions[1]);
         System.out.println(androidVersions[2]);
-        System.out.println(androidVersions[3]);
+        System.out.println(androidVersions[3]);*/
 
         System.out.println();
         System.out.println();
@@ -30,14 +38,27 @@ public class Arrays {
         cities[3][0] = "Mexico";
         cities[3][1] = "CDMX";
 
-        System.out.println(cities[0][0]);
+        /*System.out.println(cities[0][0]);
         System.out.println(cities[0][1]);
         System.out.println(cities[1][0]);
         System.out.println(cities[1][1]);
         System.out.println(cities[2][0]);
         System.out.println(cities[2][1]);
         System.out.println(cities[3][0]);
-        System.out.println(cities[3][1]);
+        System.out.println(cities[3][1]);*/
+
+        /*for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[i].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+
+        }*/
+
+        for (String[] pair: cities) {
+            for (String name:pair) {
+                System.out.println(name);
+            }
+        }
 
         String[][][][] animals = new String[2][3][2][2];
         animals[1][0][0][1] = "Monkey";
@@ -45,6 +66,20 @@ public class Arrays {
         System.out.println();
         System.out.println();
         System.out.println(animals[1][0][0][1]);
+
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j <= 0; j++) {
+                for (int k = 0; k <= 0; k++) {
+                    for (int l = 0; l <=1 ; l++) {
+                        System.out.println(animals[i][j][k][l]);
+
+                    }
+
+                }
+
+            }
+
+        }
 
     }
 }
